f76014921f66bb76b4f1add173da24cf5d8fe3f6
Linet Oliech <linetaoliech@gmail.com>
2022-11-24 22:22:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -24,6 +24,20 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+//strings
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+const [bessie , , dolly, babe, little] = farmAnimals.split(' ');
+const [blackAndWhite, , black, pink, ] = farmAnimals.split(' ');
+
+//Arrays
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, , v] = colors;
+const [ , , , , , indg, ] = colors;
+
+//Objects
+const {muppetName, color, song, job, partner} = muppet;
+const {song2, song4} = nestedMuppet.album.theMuppetMovie;
+const {nestedJob, nestedPartner} = nestedMuppet;
 
 // Strings
 
